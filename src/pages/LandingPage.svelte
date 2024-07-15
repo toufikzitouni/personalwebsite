90b434b91b2de973cab374c9803ab1d499f7a410
toufikzitouni <toufik_z@hotmail.com>
2024-07-15 06:56:16 -0700

    Add resume link

--- src/pages/LandingPage.svelte
+++ src/pages/LandingPage.svelte
@@ -82,6 +82,29 @@
           >
             <span>Hobbies</span>
           </button>
+          <div class="h-8"></div>
+          <a
+            href="./assets/files/document.pdf"
+            target="_blank"
+            rel="noopener noreferrer"
+            class="flex items-center space-x-2 font-poppins font-semibold text-sm tracking-wide text-slate-500 hover:text-slate-300"
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke-width="1.5"
+              stroke="currentColor"
+              class="w-5 h-5"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                d="M3 16.5v2.25A2.25 2.25 0 0 0 5.25 21h13.5A2.25 2.25 0 0 0 21 18.75V16.5M16.5 12 12 16.5m0 0L7.5 12m4.5 4.5V3"
+              />
+            </svg>
+            <span>Resume</span>
+          </a>
         </div>
       </div>
       <div class="flex items-start">
